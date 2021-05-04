57a8a8a61e70940e7d027c94954e1531c67dfd35
CaubiLoureiro <79065406+CaubiLoureiro@users.noreply.github.com>
2021-05-03 22:51:42 -0300

    Update Main.java

--- tarefa07/questao01/Main.java
+++ tarefa07/questao01/Main.java
@@ -1,4 +1,4 @@
-package tarefa7.questao01;
+package tarefa07.questao01;
 
 import tarefa07.questao01.ThreadPrimos;
 
@@ -7,7 +7,16 @@ public class Main {
         QuantidadeDePrimos objeto = new QuantidadeDePrimos(10);
         ThreadPrimos thread1 = new ThreadPrimos(objeto, 0);
         ThreadPrimos thread2 = new ThreadPrimos(objeto, 1);
+        Thread t1 = new Thread(thread1);
+        Thread t2 = new Thread(thread2);
+        t1.start();
+        t2.start();
 
-        System.out.println(QuantidadeDePrimos.qtdPrimos);
+        while(t1.isAlive() || t2.isAlive()){
+            continue;
+        }
+
+        System.out.println(QuantidadeDePrimos.getQtdPrimos());
+
     }
 }
