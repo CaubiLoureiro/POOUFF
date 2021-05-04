2c8e3978aa97e8d35469469d58a174521bb5bd86
CaubiLoureiro <79065406+CaubiLoureiro@users.noreply.github.com>
2021-05-03 22:52:00 -0300

    Update ThreadPrimos.java

--- tarefa07/questao01/ThreadPrimos.java
+++ tarefa07/questao01/ThreadPrimos.java
@@ -1,15 +1,16 @@
 package tarefa07.questao01;
 
-import tarefa7.questao01.QuantidadeDePrimos;
+import tarefa07.questao01.QuantidadeDePrimos;
 
 public class ThreadPrimos implements Runnable{
     QuantidadeDePrimos objeto;
     private int pegaMetade;
 
+
     public ThreadPrimos(QuantidadeDePrimos objeto, int metade){
         this.objeto = objeto;
         this.pegaMetade = metade;
-        new Thread(this).start();
+
     }
 
 
@@ -17,29 +18,47 @@ public class ThreadPrimos implements Runnable{
     @Override
     public void run() {
         if (pegaMetade == 1) {
+
             int metade = (objeto.getNumeroMax()/2) + 1;
             for(int i = metade; i<= objeto.getNumeroMax(); i++) {
+                int divisores = 0;
+                for(int j=1; j<=i; j++){
 
-                if(i!=1){// IF para no considerar o 1 como primo
-                    if (i % 2 != 0 || i % 3 != 0 || i == 2 || i == 3) {// Se o resto da divisao por 2 e por 3 for diferente de 0 ento  primo, ou se for 2 e 3.
-                        QuantidadeDePrimos.qtdPrimos++;
+                    if (i%j == 0){
+
+                        ++divisores;
+
                     }
                 }
+                if(divisores == 2){
+                    objeto.somaPrimos();
+
+                }
             }
+
         } else {
             // Repete o mesmo processo do IF acima, porem para a outra metade do numero.
             int metade = objeto.getNumeroMax()/2;
-            for(int i=0; i<=metade; i++){
-                if(i!=1){
-                    if (i % 2 != 0 || i % 3 != 0 || i == 2 || i == 3) {
-                        QuantidadeDePrimos.qtdPrimos++;
-                    }
-                }
 
+            for(int i=1; i<=metade; i++){
+                int divisores = 0;
+                for(int j=1; j<=i; j++){
+                    if (i%j == 0){
+
+                        ++divisores;
+                    }
+
+                }
+                if(divisores == 2){
+                    objeto.somaPrimos();
+
+                }
             }
+
 
         }
 
-
     }
+
+
 }
